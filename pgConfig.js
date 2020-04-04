f5c2a87f9d4bc53caa2e13b0c47471a5fc4035cf
npezzotti <nathan.pezzotti@datadoghq.com>
2020-04-04 11:42:23 -0400

    added gitignore

--- pgConfig.js
+++
@@ -1,9 +0,0 @@
-const Pool = require('pg').Pool;
-
-module.exports = pool = new Pool({
-    user: 'nathan',
-    host: 'localhost',
-    database: 'api',
-    password: 'postgres',
-    port: 5432
-});
