af02e1e4cd4c5439c98d5c6820cb03019b68fc71
npezzotti <nathan.pezzotti@datadoghq.com>
2020-04-04 22:43:38 -0400

    get rid of console.logs

--- usersController.js
+++ usersController.js
@@ -4,7 +4,6 @@ const getUsers = (req, res) => {
     pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
         if (error) throw error;
         res.status(200).json(results.rows);
-        console.log(results)
     });
 };
 
